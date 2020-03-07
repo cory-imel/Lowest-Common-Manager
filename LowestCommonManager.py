2b03fbaa113f1954fde6858bb23ccc464292b9ad
Cory Imel <11916295+xoxide@users.noreply.github.com>
2020-03-07 15:28:03 -0700

    Initial

---
+++ LowestCommonManager.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Cory Imel"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+from logzero import logger
+from anytree import Node, RenderTree
+
+
+class LowestCommonManager(object):
+
+    def __init__(self, hierarchy, root, group):
+
+            self.hierarchy = hierarchy
+            self.root = root
+            self.group = group
+
+    def build_tree(self):
+
+        logger.info("Building Tree")
+
+        tree = {}
+
+        tree[self.root[0]] = (Node(self.root[0], parent=None))
+
+        for manager in self.hierarchy:
+            for subordinate in self.hierarchy[manager]:
+                tree[subordinate[0]] = (Node(subordinate[0], parent=tree[manager[0]]))
+        return tree
+
+    def find_lca(self, ancestors1, ancestors2):
+
+        i = 0
+        while i < len(ancestors1) and i < len(ancestors2):
+            if ancestors1[i] != ancestors2[i]:
+                break
+            i += 1
+        return ancestors1[i - 1].name
+
+    def recurse_lca(self, tree, group, lcm):
+
+        if lcm is self.root[0]:
+            return self.root[0]
+
+        if len(group) is 1:
+
+            if lcm is tree[group[0]].parent.name:
+                return lcm
+
+            ancestors1 = tree[group[0]].ancestors
+            ancestors2 = tree[lcm].ancestors
+            return self.find_lca(ancestors1, ancestors2)
+
+        elif len(group) is 2:
+
+            ancestors1 = tree[group[0]].ancestors
+            ancestors2 = tree[group[1]].ancestors
+            return self.find_lca(ancestors1, ancestors2)
+
+        else:
+
+            ancestors1 = tree[group.pop()].ancestors
+            ancestors2 = tree[group.pop()].ancestors
+            lcm = self.find_lca(ancestors1, ancestors2)
+            return self.recurse_lca(tree, group, lcm)
---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Cory Imel"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import argparse
+import getpass
+import re
+from logzero import logger
+from LDAPSearcher import LdapSearcher
+from LowestCommonManager import LowestCommonManager
+
+
+def main(args):
+
+    group = ['Stella Wiley', 'Ronni Ko', "Bob Johnson"]
+
+    domain = re.search('ldaps://((.*\.?)+)', args.server)
+    split = domain.groups()[0].split('.')[1:]
+    partDomain = 'dc=' + ',dc='.join(split)
+
+    searcher = LdapSearcher(args.server, partDomain, args.user, args.password)
+    root, hierarchy = searcher.retrieve_hierarchy(args.root, args.depth)
+    lcm = LowestCommonManager(hierarchy, root, group=[])
+    tree = lcm.build_tree()
+
+    searcher.render_graph(root, hierarchy, args)
+    searcher.unbind_ldap()
+    logger.info("Starting Search")
+    print(lcm.recurse_lca(tree, group, lcm=''))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', '-s', help='The FQDN of the ldap server e.g. ldap://domain.server.com',
+                        required=True)
+    parser.add_argument('--user', '-u', help='The user to log into the ldap server with e.g. user@domain.server.com',
+                        required=True)
+    parser.add_argument('--password', '-p', help='LDAP user password.', default="")
+    parser.add_argument('--root', '-r', help='The root user''s login name e.g. name.surname', required=True)
+    parser.add_argument('--file', '-f', help='The file to output to e.g. c:\chart.png', required=True)
+    parser.add_argument('--depth', '-d', type=int, default=max,
+                        help='The amount of levels to represent in the hierarchy.')
+    parser.add_argument('--imageType', '-i', help='An image type understood by GraphViz e.g. png, svg, etc.')
+    parser.add_argument('--layout', '-l', help='A layout type understood by GraphViz e.g. dot, neato, circo, etc.')
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+
+    if args.password is "":
+        args.password = getpass.getpass("Password: ", stream=None)
+
+    main(args)
